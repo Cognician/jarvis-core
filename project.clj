f1c7138ab3a63d6eebf6f5557108704a4806f29e
Robert Stuttaford <robert@cognician.com>
2014-03-30 11:30:36 +0200

    Update tools.reader dep.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [com.rallydev/clj-flowdock "1.2.2"]
                  [org.clojure/java.classpath "0.2.0"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.reader "0.7.5"]
+                 [org.clojure/tools.reader "0.8.3"]
                  [clj-http "0.7.2"]
                  [fs "1.3.2"]
                  [ch.qos.logback/logback-classic "1.0.9"]]
