0411a86a42ae39cb821804c9e0c59c1ee2a630f8
Robert Stuttaford <robert@cognician.com>
2016-01-26 08:16:29 +0200

    Update deps.

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
-(defproject com.rallydev/jarvis-core "1.6.3"
+(defproject com.rallydev/jarvis-core "1.6.4"
   :description "A robot for FlowDock"
   :url "http://github.com/RallySoftware/jarvis-core"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [com.rallydev/clj-flowdock "1.2.2"]
-                 [org.clojure/java.classpath "0.2.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.reader "0.8.3"]
-                 [clj-http "0.7.2"]
-                 [fs "1.3.2"]
-                 [ch.qos.logback/logback-classic "1.0.9"]]
+                 [com.rallydev/clj-flowdock "1.2.4"]
+                 [org.clojure/java.classpath "0.2.3"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.reader "0.10.0"]
+                 [clj-http "2.0.1"]
+                 [fs "1.3.3"]
+                 [cheshire "5.5.0"]
+                 [ch.qos.logback/logback-classic "1.1.3"]]
   :source-paths ["src/main/clojure" "src/plugins"]
   :java-source-paths ["src/main/java"]
   :test-paths ["src/test/clojure"]
